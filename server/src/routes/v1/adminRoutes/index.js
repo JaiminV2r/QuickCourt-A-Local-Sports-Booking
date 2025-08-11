ecb9828a07ad06c5ec8b18e5fc3bad438f166cc2
Jaimin Vaddoriya <jaiminvaddoriya194@gmail.com>
2025-08-11 22:44:46 +0530

    Create Venue module API for admin.

--- server/src/routes/v1/adminRoutes/index.js
+++ server/src/routes/v1/adminRoutes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const userRoutes = require('./user.route');
 const authRoutes = require('./auth.route');
+const venueRoutes = require('./venue.route');
 
 const router = express.Router();
 
 router.use('/auth', authRoutes); // Auth routes.
 router.use('/user', userRoutes); // User routes.
+router.use('/venue', venueRoutes); // Venue routes.
 
 module.exports = router;
