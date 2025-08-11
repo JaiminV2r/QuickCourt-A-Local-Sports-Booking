f1cc330297b0230d44d69152c8cac59c73630182
Dhruvkachwala <pateldhruv5536@gmail.com>
2025-08-12 03:05:17 +0530

    solving auth error

--- server/src/routes/v1/adminRoutes/dashboard.route.js
+++ server/src/routes/v1/adminRoutes/dashboard.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const auth = require('../../../middlewares/auth');
+const {auth} = require('../../../middlewares/auth');
 const { dashboardController } = require('../../../controllers/adminControllers');
 const validate = require('../../../middlewares/validate');
 const { ROLES } = require('../../../helper/constant.helper');
